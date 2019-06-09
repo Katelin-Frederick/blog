26028c4f7166126bb06fe31244f1d705dc773247
Katelin Frederick <k.frederick94@gmail.com>
2019-06-09 09:51:21 -0400

    edited footer

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 import Header from '../components/Header'
-import Footer from '../components/footer'
+import Footer from '../components/Footer'
 import '../syles/index.scss'
 import layoutStyles from './Layout.module.scss'
 
